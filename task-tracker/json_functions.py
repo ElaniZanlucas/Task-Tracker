0015f0feb2ed8a43dd807e8db9b73b2956a0e006
elani <emzan.06@gmail.com>
2025-03-26 12:12:55 -0400

    Tabulate and documentation implemented. Project finished!

--- task-tracker/json_functions.py
+++ task-tracker/json_functions.py
@@ -4,23 +4,30 @@ import os
 file_path = 'tasks.json'
 
 def get_json():
+    """
+    Create a file if doesn't exist and read a json file
+    :return: list of dict
+    """
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
             try:
                 data = json.load(file)
             except json.JSONDecodeError:
-                data = []  # Se o arquivo estiver vazio ou corrompido, começamos com uma lista vazia
+                data = []
     else:
         data = []
 
-    # Garantimos que os dados sejam uma lista antes de adicionar o novo dado
     if not isinstance(data, list):
         data = [data]
 
     return data
 
 def set_json(data):
-    # Write data
+    """
+    Write data into a json file
+    :param data: list of dict
+    :return: None
+    """
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
